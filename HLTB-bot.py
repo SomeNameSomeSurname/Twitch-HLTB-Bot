ac1165834c36853b7d6d77c7b50fdef8852f67f6
SomeName <titovdaniils@outlook.com>
2025-05-02 20:30:16 +0500

    no message

--- HLTB-bot.py
+++ HLTB-bot.py
@@ -7,33 +7,36 @@ import asyncio
 class HLTBBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            token="YOUR_TOKEN",
-            prefix="!",
-            initial_channels=['CHOOSE_CHANNELS']
+            token="YOUR_TOKEN", #paste here you token
+            prefix="!", #You can keep it, or change for any other
+            initial_channels=['CHOOSE_CHANNELS'] #channels that you want to work with
         )
 
     async def event_ready(self):
         pass
 
+    #Ping command check
     @commands.command(name='pingHLTB')
     async def ping_command(self, ctx):
         await ctx.send("Ping ppHop Pong")
 
+    #HLTB search command
     @commands.command(name='hltb')
     async def hltv_command(self, ctx, *, game_name: str):
         try:
             results = await HowLongToBeat().async_search(game_name)
 
             if results is None or len(results) == 0:
-                await ctx.send(f"Нет игры NuAHule")
+                await ctx.send(f"No game found")
                 return
 
             best_match = max(results, key=lambda  x: x.similarity)
 
+            #Comment section of the match if needed
             message = (
-                f"Игра: {best_match.game_name} ({best_match.release_world}) | "
-                f"Main Story: {best_match.main_story} ч. | "
-                f"Completionist: {best_match.completionist} ч. | "
+                f"Game: {best_match.game_name} ({best_match.release_world}) | "
+                f"Main Story: {best_match.main_story} H. | "
+                f"Completionist: {best_match.completionist} H. | "
                 f"Link: {best_match.game_web_link}"
             )
 
@@ -41,7 +44,7 @@ class HLTBBot(commands.Bot):
 
         except Exception as e:
             print(f"Error: {e}")
-            await ctx.send("Я поломался oh ")
+            await ctx.send("I'm broken oh ")
 
 if __name__ == "__main__":
     bot = HLTBBot()
